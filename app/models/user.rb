0b92e630db7397a3a6c37495d4ac90b4aa98b134
teenyTECHmama <116871259+teenyTECHmama@users.noreply.github.com>
2023-05-11 06:31:27 +0200

    added user database and validations

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+#name:string 
+#email:string 
+#password_digest:string 
+#games_per_day:integer
+#
+#password:string virtual
+#password_confirmation:string virtual
+class User < ApplicationRecord
+    has_secure_password
+    validates :name, presence: true
+    validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address" }
+    validates :games_per_day, presence: true
+
+end
---
+++ db/migrate/20230511035150_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email, null: false
+      t.string :password_digest
+      t.integer :games_per_day
+
+      t.timestamps
+    end
+  end
+end
