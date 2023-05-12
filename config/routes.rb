c45eacace94f69960b8af4b5d6464c1a673ade82
teenyTECHmama <116871259+teenyTECHmama@users.noreply.github.com>
2023-05-12 07:11:17 +0200

    setting up signup -not rendring errors or to html registration#create

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
   # GET /home
-  get "home", to: "home#index", as: :home
-  # Defines the root path route ("/")
+  get "home", to: "home#index"
+  # Defines the root path route ("/") 
+
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+
   root to: "main#index"
   # root "articles#index"
 end
